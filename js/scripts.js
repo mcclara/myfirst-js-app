176bae5b89c8d956f9eeb96fffbdbe59f4f20357
mcclara <angelicavmcclary@gmail.com>
2020-11-21 18:08:38 -0500

    Update scripts.js
    Fixed syntax errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
-  {name: 'Ninetales', height: 1, types = ['Fire'];},
-  {name: 'Butterfree', height: 1, types = ['Bug', 'Flying'];},
-  {name: 'Jigglypuff', height: 1, types = ['Fairy', 'Normal'];},
-  {name: 'Deerling', height: 1, types =  ['Grass', 'Normal'];}
+  {name: 'Ninetales', height: 1, types: ['Fire']},
+  {name: 'Butterfree', height: 1, types: ['Bug', 'Flying']},
+  {name: 'Jigglypuff', height: 1, types: ['Fairy', 'Normal']},
+  {name: 'Deerling', height: 1, types: ['Grass', 'Normal']}
 ];
