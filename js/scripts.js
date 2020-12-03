ad62facc5c7da940d3cbaaba8c0ee5d6a4a37b24
mcclara <angelicavmcclary@gmail.com>
2020-12-03 01:52:53 -0500

    Update scripts.js
    Added functions  for getAll and add

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,24 @@
 /*jshint esversion: 6 */
+let pokemonRepository = (function() {
 let pokemonList = [
   {name: 'Ninetales', height: 1.2, types: ['Fire']},
   {name: 'Butterfree', height: 1.1, types: ['Bug', 'Flying']},
   {name: 'Jigglypuff', height: 0.5, types: ['Fairy', 'Normal']},
   {name: 'Deerling', height: 0.6, types: ['Grass', 'Normal']}
 ];
+function add(pokemon) {
+    pokemonRepository.add(item);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+}) ();
 
 pokemonList.forEach(function(pokemon) {
   document.write(pokemon.name + ' is ' + pokemon.height + ' and is ' + pokemon.types + " ");
